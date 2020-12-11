14f8ca330030540954f333058e6ddf5ab7a70e75
riccardo berto <ricki1993.scorpione@gmail.com>
2020-12-11 19:48:01 +0100

    Parallel to sequencial working

--- max_clique.cu
+++ max_clique.cu
@@ -15,19 +15,16 @@
 
 
 constexpr int N = 500;
-constexpr int RATE = 30;
+constexpr int RATE = 45;
 // flag =0 solo sequenziale, flag =1 entrambi, flag >1 solo parallelo
 constexpr int flag = 2;
 
 //per sincronizzare eseguo atomicexec()
 __device__ int d_best_size[N];
 __device__ int dev_max_clique[N * N];
-__device__ int deepest[N];
 
 
 cudaError_t clique_launcher(const std::vector<int>& degrees, const std::vector<int>& neighbours, const std::vector<int>& indexes);
-
-
 
 
 void rec_seq_clique(const std::vector<int> &degrees, const std::vector<int>&neighbours, std::deque<int> &intersection,const std::vector<int>&indexes, std::array<int, N> &max_clique_seq, std::array<int,N> &tmp, int &best_size, int tmp_index, int current) {
@@ -74,8 +71,8 @@ void seq_clique(const std::vector<int> &degrees, const std::vector<int>&neighbou
     best_size = 0;
     for (int i = 0; i < N; i++) {
         tmp[0] = i;
-        
-        rec_seq_clique(degrees, neighbours, std::deque<int>(0), indexes, max_clique_seq, tmp, best_size, 1, i);
+        std::deque<int> d = std::deque<int>(0);
+        rec_seq_clique(degrees, neighbours, d, indexes, max_clique_seq, tmp, best_size, 1, i);
         
     }
 }
@@ -92,7 +89,7 @@ std::vector<int> create_graph(std::vector<int>& degrees, int MaxNeightbours) {
             if (r < RATE) {
                 if (degrees[i] >= MaxNeightbours || degrees[j] >= MaxNeightbours) printf("Troppi vicini, balzo\n");
                 else {
-                    printf("%d, %d, %d, %d\n", i, j, (i * MaxNeightbours) + degrees[i], (j * MaxNeightbours) + degrees[j]);
+                    //printf("%d, %d, %d, %d\n", i, j, (i * MaxNeightbours) + degrees[i], (j * MaxNeightbours) + degrees[j]);
                     temp_neighbours[i * MaxNeightbours + degrees[i]] = j;
                     temp_neighbours[j * MaxNeightbours + degrees[j]] = i;
                     degrees[i]++;
@@ -168,9 +165,9 @@ int main() {
         tmp_max_clique_seq[i] = -1;
     }
 
-    int f = 0;
+    /*int f = 0;
     int count = 0;
-   /* for (int i = 0; i < indexes.size(); i++) {
+    for (int i = 0; i < indexes.size(); i++) {
         for (int j = 0; j < degrees[i]; j++) {
             std::cout << neighbours[indexes[i]+j] << ' ';
         }
@@ -211,20 +208,29 @@ int main() {
 
 /*__device__*/ int * parallel_intersection(int *arr1, int *arr2, int x, int y, int *count) {
     int* tmp;
-    int* p= nullptr;
-    if (y > x)
+    /*if (y > x)
         tmp = new int[x];
     else
-        tmp = new int[y];
-    //int* tmp = new int[y ^ ((x ^ y) & -(x < y))];
+        tmp = new int[y];*/
+    int test = ((int)(x > y))* y + ((int)(y >= x))* x;
+    //printf("%d ", test);
+    tmp = new int[test];
     int i = 0, j = 0;
+    int  a = 0, b = 0;
     *count = 0;
     while (i < x && j < y) {
+        
+        a = i;
+        b = j;
+        //(arr1[a] < arr2[b]) ? (i = i + 1) : ((arr1[a] > arr2[b]) ? j=j+1 : 0);
+        
+        //printf("%d %d \n", i, j);
+
         if (arr1[i] < arr2[j])
             i++;
         else if (arr2[j] < arr1[i])
             j++;
-        else /* if arr1[i] == arr2[j] */
+        else
         {
             tmp[*count] = arr1[i];
             i++;
@@ -232,55 +238,52 @@ int main() {
             (*count)++;
         }
     }
-    if (*count > 0) {
-        p = new int[*count];
-        for (int k = 0; k < *count; k++) {
-            p[k] = tmp[k];
-        }
-    }   
+    int *p = new int[*count];
+    for (int k = 0; k < *count; k++) {
+        p[k] = tmp[k];
+    }
     delete[] tmp;
     return p;
 }
 
-/*__device__*/ void rec_paral(int* dev_degrees, int* dev_neighbours, int* dev_indexes, int* intersection, int inter_size, int *tmp, int tmp_indexes, int current, int global_idx) {
+/*__device__*/ void rec_paral(int* dev_degrees, int* dev_neighbours, int* dev_indexes, int* intersection, int inter_size, int *tmp, int tmp_indexes, int current, int global_idx, int *local_max, int *to_ret) {
+    
     int* next_inter = nullptr;
     int size=0, i = 0;
-    deepest[global_idx] = tmp_indexes;
-    while (i < inter_size) {
+    while (i < inter_size && (*local_max) < inter_size - i + tmp_indexes) {
         next_inter = parallel_intersection(intersection, &(dev_neighbours[dev_indexes[intersection[i]]]), inter_size, dev_degrees[current], &size);
         tmp[tmp_indexes] = intersection[i];
-        rec_paral(dev_degrees, dev_neighbours, dev_indexes, next_inter, size, tmp, tmp_indexes+1, intersection[i], global_idx);
+        rec_paral(dev_degrees, dev_neighbours, dev_indexes, next_inter, size, tmp, tmp_indexes+1, intersection[i], global_idx, local_max, to_ret);
         i++;
     }
     
-    if (tmp_indexes > d_best_size[global_idx]) {
-        d_best_size[global_idx] = tmp_indexes;
+    if (tmp_indexes > *local_max /*d_best_size[global_idx]*/) {
+        //d_best_size[global_idx] = tmp_indexes;
+        *local_max = tmp_indexes;
         for (int j = 0; j < tmp_indexes; j++) {
-            dev_max_clique[(N * (global_idx)) + j] = tmp[j];
+            //dev_max_clique[(N * (global_idx)) + j] = tmp[j];
+            to_ret[j]=tmp[j];
         }      
     }
     
 }
 
 
-/*__global__*/ void paral_max_clique(int *dev_degrees,int *dev_neighbours, int* dev_indexes, int n_size)
+/*__global__*/ void paral_max_clique(int *dev_degrees,int *dev_neighbours, int* dev_indexes)
 {
-    /*int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    
-    int local_idx = threadIdx.x;*/
-    int global_idx = 0;
-    int tmp[N];
-    for (int i = 0; i < N; i++) {
-        tmp[i] = -1;
-    }
-    int tmp_index = 1;
+    int global_idx = 0;//blockIdx.x * blockDim.x + threadIdx.x;
+    int *tmp = new int[N];
+    int* to_ret = new int[N];
     tmp[0] = global_idx;
     int *intersection= new int[dev_degrees[global_idx]];
     for (int i = 0; i < dev_degrees[global_idx]; i++) {
         intersection[i] = dev_neighbours[dev_indexes[global_idx]+i];
-    }    
-    rec_paral(dev_degrees, dev_neighbours, dev_indexes, intersection, dev_degrees[global_idx], tmp, 1, global_idx, global_idx);
-    
+    }
+    int local_max = 1;
+    rec_paral(dev_degrees, dev_neighbours, dev_indexes, intersection, dev_degrees[global_idx], tmp, 1, global_idx, global_idx, &local_max, to_ret);
+    d_best_size[global_idx] = local_max;
+    for (int i = 0; i < local_max; i++)
+        dev_max_clique[(global_idx) + (i*N)] = to_ret[i];
     delete[] intersection;
 
 }
@@ -290,43 +293,36 @@ int main() {
 //qui eseguirò il codice parallelo
 cudaError_t clique_launcher(const std::vector<int>& degrees, const std::vector<int>& neighbours, const std::vector<int>& indexes)
 {
-    int to_ret[N*N] = { -1 };
+    //int to_ret[N*N] = { -1 };
     int *dev_degrees;
     int *dev_indexes;
     int* dev_neighbours;
-    int tmp[N];
-    int sz = neighbours.size();
+    //int tmp[N];
+    int sz = (int)neighbours.size();
     int d[N];
     int in[N];
-
-    for (int i = 0; i < N * N; i++) {
+    int i = 0, out = 0, max=0;
+    std::chrono::steady_clock::time_point begin, end;
+    std::chrono::duration<float, std::milli> ms;
+   /* for (int i = 0; i < N * N; i++) {
         to_ret[i] = -1;
     }
     for (int i = 0; i < N; i++) {
         tmp[i] = -1;
-    }
+    }*/
     int* n = (int*)malloc(neighbours.size() * sizeof(int));
-    cudaError_t cudaStatus = cudaSuccess;
+    cudaError_t cudaStatus= cudaSuccess;
 
     for (int i = 0; i < N; i++) {
         d[i] = degrees[i];
         in[i] = indexes[i];
     }
-    for (int i = 0; i < neighbours.size(); i++) {
+    for (int i = 0; i < (int) neighbours.size(); i++) {
         n[i] = neighbours[i];
     }
-    paral_max_clique(d, n,in, 0);
-    printf("\n");
-    
-    printf("%d -> ", d_best_size[0]);
-    
-    printf("\n\n");
-    for (int i = 0; i < d_best_size[0]; i++) {
-        if (i % N == 0)
-            printf("  ||  ");
-        printf("%d ->", dev_max_clique[i]);
-        
-    }
+    begin = std::chrono::steady_clock::now();
+    paral_max_clique(d, n, in);
+    printf("finito\n");
     /*
     cudaStatus = cudaMalloc((void**)&dev_degrees, N * sizeof(int));
     if (cudaStatus != cudaSuccess) {
@@ -340,11 +336,6 @@ cudaError_t clique_launcher(const std::vector<int>& degrees, const std::vector<i
         goto Error;
     }
 
-    cudaStatus = cudaMalloc((void**)&deepest, N * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
 
     cudaStatus = cudaMalloc((void**)&dev_max_clique, N*N * sizeof(int));
     if (cudaStatus != cudaSuccess) {
@@ -394,15 +385,10 @@ cudaError_t clique_launcher(const std::vector<int>& degrees, const std::vector<i
         fprintf(stderr, "cudaMemcpytosymbol failed!", cudaGetErrorString(cudaStatus));
         goto Error;
     }
-    cudaStatus = cudaMemcpyToSymbol(deepest, tmp, N * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpytosymbol failed!", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
     printf("\n\navvio kernel\n\n ");
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    begin = std::chrono::steady_clock::now();
     //qua lancio il kernel
-    paral_max_clique<<<1, N>>> (dev_degrees, dev_neighbours, dev_indexes, sz);
+    paral_max_clique<<<1, 1>>> (dev_degrees, dev_neighbours, dev_indexes, sz);
     
     //controllo errori nel lancio dle kernel
     cudaStatus = cudaGetLastError();
@@ -411,18 +397,17 @@ cudaError_t clique_launcher(const std::vector<int>& degrees, const std::vector<i
         goto Error;
     }
 
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::chrono::duration<float, std::milli> ms = end - begin;
-    std::cout << "\n\nTime difference = " << ms.count() << "[ms] " << ms.count() / 1000 << "[s] " << ms.count() / 60000 << "[m] " << std::endl;
-
     // cudaDeviceSynchronize waits for the kernel to finish, and returns
     // any errors encountered during the launch.
     cudaStatus = cudaDeviceSynchronize();
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n %s\n", cudaStatus, cudaGetErrorString(cudaStatus));
         goto Error;
-    }
-
+    }*/
+    end = std::chrono::steady_clock::now();
+    ms = end - begin;
+    std::cout << "\n\nTime difference = " << ms.count() << "[ms] " << ms.count() / 1000 << "[s] " << ms.count() / 60000 << "[m] " << std::endl;
+    /*
     cudaStatus = cudaMemcpyFromSymbol(to_ret, dev_max_clique, N * N * sizeof(int));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpyFromSymbol failed!");
@@ -434,10 +419,12 @@ cudaError_t clique_launcher(const std::vector<int>& degrees, const std::vector<i
         goto Error;
     }
     printf("\n d_best: \n");
-    for (int i = 0; i < N; i++) {
+    for (i = 0; i < N; i++) {
         printf("%d -> ", tmp[i]);
     }
-    int i = 0, out = 0, max=0;
+    i = 0;
+    out = 0;
+    max = 0;
     while (i < N && out == 0) {
         max = tmp[i] > max ? i : max;
         i++;
@@ -448,27 +435,18 @@ cudaError_t clique_launcher(const std::vector<int>& degrees, const std::vector<i
 
         printf("%d -> ", to_ret[(N*max)+i]);
     }
-    /*for (i = 0; i < N * N; i++) {
-        printf("%d -> ", to_ret[i]);
-    }
 
 Error:
-    cudaStatus = cudaMemcpyFromSymbol(tmp, deepest, N * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpyFromSymbol failed! %s", cudaGetErrorString(cudaStatus));
-    }
-    printf("deepest:\n");
-    for (i = 0; i < N; i++) {
-
-        printf("%d: %d -> ", i,to_ret[i]);
-    }
     cudaFree(d_best_size);
     cudaFree(dev_max_clique);
-    cudaFree(deepest);
     cudaFree(dev_degrees);
     cudaFree(dev_neighbours);
     cudaFree(dev_indexes);*/
-        
+    free(n);
+    printf("\n");
+    for (int i = 0; i < d_best_size[0]; i++) {
+        printf("%d -> ", dev_max_clique[N*i]);
+    }
     return cudaStatus;
 }
 
